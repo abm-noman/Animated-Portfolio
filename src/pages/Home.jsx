cada95767dcb08ac3dd73b97e492061aa39abb98
abm-noman <noman15-2839@diu.edu.bd>
2026-07-28 09:53:21 +0600

    3D Objects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from "../models/island";
+import Sky from "../models/Sky";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null; 
+    let screenPosition = [0, -6.5, -43];
+    let screenRotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43];
+      screenRotation = [0.1, 4.7, 0];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [ screenScale, screenPosition, screenRotation ];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen flex items-center justify-center">
       {/* <div className="absolute top-28 left-0 right-o z-10 flex items-center justify-center">
@@ -19,6 +39,13 @@ const Home = () => {
           <pointLight position={[10, 10, 10]} intensity={1} />
           <spotLight position={[0, 10, 0]} angle={0.3} penumbra={1} intensity={2} castShadow />
           <hemisphereLight skyColor={0xffffff} groundColor={0x444444} intensity={0.5} />
+
+          <Sky />
+
+          <Island 
+          position={islandPosition}
+          scale={islandScale}
+          screenRotation={islandRotation}/>
         </Suspense>
 
       </Canvas>
